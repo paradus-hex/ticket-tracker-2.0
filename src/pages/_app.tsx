423e6e5167e476ae0af769a945898153d067ac20
Yaseen Nur <paradushex@gmail.com>
2023-03-27 17:56:18 +0600

    added dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,29 @@ import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
   );
 };
 
