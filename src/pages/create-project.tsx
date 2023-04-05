39aeadf36e4216f6f16cd94ebaa75c84ecb40281
Yaseen Nur <paradushex@gmail.com>
2023-04-05 22:02:09 +0600

    create projects

--- src/pages/create-project.tsx
+++ src/pages/create-project.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ProtectedLayout from "./components/ProtectedLayout";
-import { TextField, Box, Stack, Typography } from "@mui/material";
+import { TextField, Box, Stack, Typography, Button } from "@mui/material";
 import { api } from "~/utils/api";
+import { type CreateProjectPayloadType } from "~/server/api/controllers/project.controller";
+import { useRouter } from "next/router";
 const CreateProject = () => {
   const { mutateAsync } = api.project.createProject.useMutation();
+  const router = useRouter();
+
+  const [projectPayload, setProjectPayload] =
+    useState<CreateProjectPayloadType>({} as CreateProjectPayloadType);
+
+  const handleSubmit = async () => {
+    try {
+      await mutateAsync(projectPayload);
+      await router.push("/projects");
+      return;
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <ProtectedLayout>
       <Stack className="flex items-center justify-center" gap={5}>
         <Typography variant="h5">Create A Project</Typography>
         <TextField
-          helperText="Please enter the title of the project"
+          helperText="Enter the title of the project"
           id="project-title"
           label="Title"
           variant="filled"
           sx={{ width: "20%" }}
+          onChange={(e) => {
+            setProjectPayload({ ...projectPayload, title: e.target.value });
+          }}
         />
         <TextField
           helperText="Please enter the project description "
@@ -23,7 +42,16 @@ const CreateProject = () => {
           multiline
           rows={5}
           sx={{ width: "30%" }}
+          onChange={(e) => {
+            setProjectPayload({
+              ...projectPayload,
+              description: e.target.value,
+            });
+          }}
         />
+        <Button variant="outlined" onClick={handleSubmit}>
+          Submit
+        </Button>
       </Stack>
     </ProtectedLayout>
   );
--- src/server/api/controllers/project.controller.ts
+++ src/server/api/controllers/project.controller.ts
@@ -3,11 +3,19 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import projectModel from "../models/project.model";
 import { z } from "zod";
 
+const createProjectPayloadSchema = z.object({
+  title: z.string().max(100),
+  description: z.string(),
+});
+export type CreateProjectPayloadType = z.infer<
+  typeof createProjectPayloadSchema
+>;
+
 export const projectController = createTRPCRouter({
   getAllProjects: protectedProcedure.query(({ ctx }) =>
     projectModel.getAllProjects(ctx)
   ),
   createProject: protectedProcedure
-    .input(z.object({ title: z.string().max(100), description: z.string() }))
+    .input(createProjectPayloadSchema)
     .mutation(({ input, ctx }) => projectModel.createProject(input)),
 });
