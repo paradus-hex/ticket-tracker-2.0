4544c3d503583b71046d4028ab2978eda7676e4c
Yaseen Nur <paradushex@gmail.com>
2023-04-06 06:16:45 +0600

    models for users

--- src/server/api/controllers/user.controller.ts
+++ src/server/api/controllers/user.controller.ts
@@ -1,13 +1,16 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 import { z } from "zod";
-import { prisma } from "~/server/db";
 import userModel from "../models/user.model";
 
 const userRoleUpdatePayloadSchema = z.object({
   id: z.string(),
   role: z.enum(["ADMIN", "USER"]),
 });
+
+const getUserByIdPayloadSchema = z.object({ id: z.string() });
+
+export type GetUserByIdPayloadType = z.infer<typeof getUserByIdPayloadSchema>;
 
 export type UserRoleUpdatePayloadType = z.infer<
   typeof userRoleUpdatePayloadSchema
@@ -19,35 +22,8 @@ export const userController = createTRPCRouter({
   ),
   updateRole: protectedProcedure
     .input(userRoleUpdatePayloadSchema)
-    .mutation(async ({ input, ctx }) => {
-      // Fetch the user's role from the database
-      const currentUser = await prisma.user.findUnique({
-        where: {
-          id: ctx.session.user.id,
-        },
-        select: {
-          role: true,
-        },
-      });
-
-      // Check if the user is an admin
-      if (currentUser?.role !== "ADMIN") {
-        throw new Error("Unauthorized: Only admins can update user roles.");
-      }
-
-      // Update the user role
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: input.id,
-        },
-        data: {
-          role: input.role,
-        },
-      });
-
-      return updatedUser;
-    }),
+    .mutation(async ({ input, ctx }) => userModel.updateRole(input, ctx)),
   getUserById: protectedProcedure
-    .input(z.object({ id: z.string() }))
-    .query(({ input }) => prisma.user.findFirst({ where: { id: input.id } })),
+    .input(getUserByIdPayloadSchema)
+    .query(({ input }) => userModel.getUserById(input)),
 });
--- src/server/api/models/user.model.ts
+++ src/server/api/models/user.model.ts
@@ -1,7 +1,42 @@
+import { prisma } from "~/server/db";
+import {
+  type GetUserByIdPayloadType,
+  type UserRoleUpdatePayloadType,
+} from "../controllers/user.controller";
 import { type ctxType } from "./../../../utils/types";
 
 const userModel = {
   getAllUsers: (ctx: ctxType) => ctx.prisma.user.findMany(),
+
+  updateRole: async (input: UserRoleUpdatePayloadType, ctx: ctxType) => {
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        id: ctx.session!.user.id,
+      },
+      select: {
+        role: true,
+      },
+    });
+
+    // Check if the user is an admin
+    if (currentUser?.role !== "ADMIN") {
+      throw new Error("Unauthorized: Only admins can update user roles.");
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: input.id,
+      },
+      data: {
+        role: input.role,
+      },
+    });
+
+    return updatedUser;
+  },
+
+  getUserById: (input: GetUserByIdPayloadType) =>
+    prisma.user.findFirst({ where: { id: input.id } }),
 };
 
 export default userModel;
